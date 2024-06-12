bb43d81015f95ab1a8e9c1962ff3407e481aa50e
Mainak <mainak9990@gmail.com>
2024-06-12 15:07:32 +0530

    first commit for typescript project

---
+++ src/components/InputField.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import "./styles.css";
+
+interface Props {
+  todo: string;
+  setTodo: React.Dispatch<React.SetStateAction<string>>;
+}
+
+const InputField: React.FC<Props> = ({ todo, setTodo }) => {
+    return (
+      <form className="input"
+      >
+        <input
+          type="input"
+          placeholder="Enter a Task"
+          value={todo}
+          onChange={(e) => setTodo(e.target.value)}
+          className="input__box"
+        />
+        <button type="submit" className="input_submit">
+          GO
+        </button>
+      </form>
+    );
+  };
+
+export default InputField;
